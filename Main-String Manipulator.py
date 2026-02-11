b193ad4c6da8f203a3bd8fe2d4ce7570c4736fdb
AlexW2010 <askforarose11@gmail.com>
2026-02-11 11:40:44 -0600

    Skeleton Code

---
+++ Main-String Manipulator.py
@@ -0,0 +1,31 @@
+def menu(): #creates a menu allowing the user to chooce which operation to perform
+    #input a string, reverse the string, count vowels, replace characters, analyze the string
+    #use a loop to prompt the user for operations until they choose to exit
+    pass
+
+def input_gathering():#accepts no arguements
+    #prompt the user to enter a string
+    #esnure the entered string is at least 5 characters long and only letters
+    pass
+
+def string_reversal(): #accepts no arguements
+    #Create a function that accepts a string as an argument and returns its reversed form.
+    pass
+
+def character_counter(): #accepts no arguements
+    #Create a function that accepts a string as an argument and returns two values;
+    #the number of vowels and the number of consonants in the string.
+    pass
+
+def character_replacer(): #character replacer accepts no arguements
+    #Create a function that replaces a specified character in the string with another character.
+    #Allow the user to choose the character to replace. Return the new string.
+    pass
+
+def string_anyalysis(): #string_anyalysis accepts no arguements
+    #Create a function that accepts a string as an argument and analyzes the string and returns:
+    #If the string is a palindrome
+    #How many total letters in the word
+    #How many vowels in the word
+    #How many consonants in the word
+    pass
